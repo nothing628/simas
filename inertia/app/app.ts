7c06951e9d7e8cc8b6ed81646c233018dfee7531
nothing628 <yogyphang@gmail.com>
2025-10-10 09:24:55 +0700

    add ui package

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -6,6 +6,10 @@ import { createSSRApp, h } from 'vue'
 import type { DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import DataTable from 'datatables.net-vue3'
+import DataTablesCore from 'datatables.net-dt';
+import 'datatables.net-responsive-dt';
+import 'datatables.net-scroller-dt';
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
@@ -22,6 +26,7 @@ createInertiaApp({
   },
 
   setup({ el, App, props, plugin }) {
+    DataTable.use(DataTablesCore);
     
     createSSRApp({ render: () => h(App, props) })
     
