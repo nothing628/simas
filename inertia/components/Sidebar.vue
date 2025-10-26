d14adf0130b3e2234689420fa500c9ba582846f7
nothing628 <yogyphang@gmail.com>
2025-10-26 21:20:21 +0700

    add sidebar component

---
+++ inertia/components/Sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="sidebar">
+        <!-- Slimscroll here -->
+    </div>
+</template>
--- inertia/pages/admin/index.vue
+++ inertia/pages/admin/index.vue
@@ -2,17 +2,24 @@
 import { Head } from '@inertiajs/vue3'
 import CsrfToken from '~/components/CsrfToken.vue';
 import Header from '~/components/Header.vue'
+import Sidebar from '~/components/Sidebar.vue';
 </script>
 
 <template>
 
     <Head title="Dashboard" />
-    <div>
+    <div class="main-wrapper">
         <Header />
-        Admin Page
-        <form method="post" action="/auth/logout">
-            <CsrfToken />
-            <button type="submit">Logout</button>
-        </form>
+        <Sidebar />
+
+        <div class="page-wrapper">
+            <div class="content">
+                Admin Page
+                <form method="post" action="/auth/logout">
+                    <CsrfToken />
+                    <button type="submit">Logout</button>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
