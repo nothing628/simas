1386d15d46b283101e72a40bfb85179ce36860d8
nothing628 <yogyphang@gmail.com>
2025-11-28 13:09:50 +0700

    add breadcrumb component

---
+++ inertia/components/Breadcrumb.vue
@@ -0,0 +1,22 @@
+<template>
+    <h3 class="text-xl text-semidark font-semibold page-title mb-1">Students</h3>
+    <nav>
+        <ol class="breadcrumb mb-0">
+            <li class="breadcrumb-item"><a href="/" class="">Dashboard</a></li>
+            <li class="breadcrumb-item">Students</li>
+            <li class="breadcrumb-item active" aria-current="page">Students Grid</li>
+        </ol>
+    </nav>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    headTitle: {
+        type: String,
+        default: ""
+    },
+    items: {
+        type: Array,
+        default: () => ([])
+    }
+})
+</script>
---
+++ inertia/components/ContentHead.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="block md:flex items-center justify-between mb-4">
+        <div class="my-auto mb-2">
+            <slot name="left"></slot>
+        </div>
+        <div class="flex xl:my-auto items-center flex-wrap">
+            <slot name="right"></slot>
+        </div>
+    </div>
+</template>
