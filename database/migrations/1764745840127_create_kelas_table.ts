82ef1a416b3c738d4a0f0e6c4fe644b7a7573acb
nothing628 <yogyphang@gmail.com>
2025-12-03 14:14:07 +0700

    tambah tabel tingkat dan kelas

---
+++ database/migrations/1764745840127_create_kelas_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'kelas'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('id_tingkat')
+      table.string('kode_kelas').notNullable().unique()
+      table.string('nama_kelas')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+
+      table.foreign('id_tingkat').references('id').inTable('tingkats')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
